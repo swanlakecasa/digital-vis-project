dc466a87f5e51cf74df6347b8a232f66718cd855
Dan Smith <danpaulsmith@gmail.com>
2017-05-14 23:48:45 +0100

    updates to radarchart

--- js/radarChart.js
+++ js/radarChart.js
@@ -19,7 +19,7 @@ function RadarChart(id, data, options) {
 	 opacityCircles: 0.1, 	//The opacity of the circles of each blob
 	 strokeWidth: 2, 		//The width of the stroke around each blob
 	 roundStrokes: false,	//If true the area and stroke will follow a round path (cardinal-closed)
-	 color: d3.scale.category20()	//Color function
+	 color: d3.scale.category10()	//Color function
 	};
 	
 	//Put all of the options into a variable called cfg
@@ -152,7 +152,7 @@ function RadarChart(id, data, options) {
 		.enter().append("g")
 		.attr("class", "radarWrapper");
 	
-	console.log('blobWrapper data', data);
+	// console.log('blobWrapper data', data);
 	//Append the backgrounds	
 	blobWrapper
 		.append("path")
